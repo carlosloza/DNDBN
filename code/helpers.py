1d13fc0627d2a5e73710045dfe30c74706164c2b
Carlos Loza <carlos.loza@onedrop.today>
2021-10-17 23:27:23 -0500

    Add model-specific methods
    - Add main pre-processing pipelines
    - Add hard-to-soft labels method
    - Add fast implementation of log-sum-exp
    - Change all acronyms to lowercase

--- code/helpers.py
+++ code/helpers.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
+from scipy.stats import zscore
 from typing import Optional, Dict
+from numpy import logaddexp
 
 
 def _round_up(x):
@@ -25,8 +27,7 @@ def _logging_messages(verbose: int, msg: Optional[Dict[str, str]]):
     return output
 
 
-
-def EEG_expert_labels(eeg, labels, stages):
+def eeg_expert_labels(eeg, labels, stages):
     """Segment single-channel EEG into stages according to expert labels"""
     for i, (k, v) in enumerate(stages.items()):
         s = 0
@@ -45,3 +46,48 @@ def EEG_expert_labels(eeg, labels, stages):
                 stages[k].append(eeg[idx, :])
             s += stages[k][-1].shape[0]
     return stages
+
+
+def time_embedding(eeg, ar_order):
+    # Embedding for a single recording - can be single or multi channel
+    n, n_dims = eeg.shape
+    xp_seq = np.zeros((n - ar_order, ar_order, n_dims))
+    for j in range(n - ar_order):
+        xp_seq[j, :, :] = -np.flip(eeg[j:j + ar_order, :], axis=0)
+    return xp_seq, n
+
+
+def preprocess(eeg, ar_order=0, labels=None, normalize=True):
+    # EEG is a list of multidimensional numpy arrays, labels is a list too
+    n_seq = len(eeg)
+    n = list()
+    # Assume all recordings are the same type, i.e. same number of channels/dimensions
+    if normalize is True:
+        eeg = [zscore(x, axis=0) for x in eeg]
+    xp, labels_all, y = list(), list(), list()
+    if ar_order > 0:
+        for i in range(n_seq):
+            xp_seq, n_i = time_embedding(eeg[i], ar_order)
+            xp.append(xp_seq)
+            n.append(n_i)
+            if labels is not None:
+                labels_all.append(labels[i][ar_order:].astype(np.float32))
+            y.append(eeg[i][ar_order:, ...])
+    # TODO: add non-autoregressive case
+    return xp, y, labels_all, n
+
+
+def hard2soft_labels(x, low=0.8, high=0.99):
+    num_states = x.shape[1]
+    num_states_arr = range(num_states)
+    for k in range(num_states):
+        idx = np.where(np.argmax(x, axis=1) == k)
+        s_labels = np.random.uniform(low, high, idx[0].size)
+        x[idx[0], k] = s_labels
+        x[idx[0], np.setdiff1d(num_states_arr, k)] = (1 - x[idx[0], k]) / (num_states - 1)
+    return x
+
+
+def log_sum_exp(x, axis=None):
+    """ Fast implementation of log-sum-exp (in comparison to scipy's implementation)"""
+    return logaddexp.reduce(x, axis=axis)
